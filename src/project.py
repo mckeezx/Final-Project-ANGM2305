dead2a38aaab822471e486c93e947c7f178cdbe8
Zachary McKee <mckeezx@gmail.com>
2026-04-14 09:21:57 -0500

    Added program outline

---
+++ src/project.py
@@ -0,0 +1,35 @@
+# Game Loop 
+# Player starts with $100 
+# If money is more than $0 and less than $300 play a round (Core)
+# Update money after round
+# When it ends, show win or lose
+
+# Core Loop (round)
+# Betting
+# Deal Cards
+# Check for Blackjack
+# Player Turn
+# Dealer Turn
+# Compare Results
+# Update Money
+
+# Player Loop 
+# While player is still playing:
+# show hand
+# ask what they want to do
+# Hit to add card
+# Stand to exit loop
+# Check if they bust then end turn
+
+# Dealer Logic
+# While dealer cards are less than 17:
+# draw a card
+
+# Compare Results
+# If player busts then lose
+# ELIF dealer busts then win
+# ELIF player > dealer then win
+# ELIF player < dealer then lsoe
+# ELSE push/tie
+
+
