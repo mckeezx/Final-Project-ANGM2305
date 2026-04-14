75bc8aa10a31691b9e78af3f40fde17faaeb53d7
Zachary McKee <mckeezx@gmail.com>
2026-04-14 10:05:00 -0500

    Changed abbreviations

--- src/project.py
+++ src/project.py
@@ -44,7 +44,7 @@ def create_deck():
 
     deck = []
     suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
     for suit in suits:
         for value in values:
