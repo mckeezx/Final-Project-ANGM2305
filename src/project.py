96bb1e9309815bc81e023d01afb7b6a6efe096ed
Zachary McKee <mckeezx@gmail.com>
2026-04-18 16:37:28 -0500

    Added abbreviations

--- src/project.py
+++ src/project.py
@@ -159,6 +159,7 @@ def render_card(card, hidden=False):
     # match each suit to the symbol
     symbols = {"Hearts": "♥", "Diamonds": "♦", "Clubs": "♣", "Spades": "♠"}
     # abbreviate jack, queen, king, ace
+    abbreviations = {"Jack": "J", "Queen": "Q", "King": "K", "Ace": "A"}
     # if card is hidden (dealer's card), return a special ascii card
     # set variable for card's value
     # also set variable for abbreviations with .get
