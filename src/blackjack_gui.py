7813d53d65f4d021a486a50e410f3b37dc52d04a
Zachary McKee <mckeezx@gmail.com>
2026-04-21 15:45:29 -0500

    Added lose and tie result

--- src/blackjack_gui.py
+++ src/blackjack_gui.py
@@ -156,9 +156,15 @@ def stand():
     if result == "win":
         money += bet
         update_display(draw_board_string() + f"\nYou win! +${bet}  |  Money: ${money}")
+    elif result == "lose":
+        money -= bet
+        update_display(draw_board_string() + f"\nYou lose. -${bet}  |  Money: ${money}")
     #lose, lose
     # else it's tie
+    else: 
+        update_display(draw_board_string() + f"\nPush. You keep ${bet}  |  Money: ${money}")
     # disable the buttons
+    disable_buttons()
 
 
 #display set up 
