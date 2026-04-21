b117c922cd205927ad6db1c5a953b2c42a7bbc41
Zachary McKee <mckeezx@gmail.com>
2026-04-21 13:39:25 -0500

    Outlined button set up

--- src/blackjack_gui.py
+++ src/blackjack_gui.py
@@ -11,10 +11,26 @@ window.geometry("960x540")
 window.configure(bg="black")
 
 #display set up 
+display = tk.Text(
 # bkg black
+bg = "black",
 # fg green
+fg = "green",
 # courier font
+font=("Courier", 12),
 # disabled state so no typing
+state = "disabled"
+width = 100
+height = 100
+)
 # display pack pady=10
+display.pack(pady=10) # 10 pixel padding top and bottom
 
 #button set up
+# tk.Frame, same bg
+# pack frame into block
+# hit button 
+# stand button
+# surrender button
+# all buttons will be their own frame with white text gray bg 
+# use grid format to format them
