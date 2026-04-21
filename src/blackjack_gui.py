36afdcd32afae62a76e60a55f27e15d5aae726d7
Zachary McKee <mckeezx@gmail.com>
2026-04-21 13:12:33 -0500

    Imported pygame functions

--- src/blackjack_gui.py
+++ src/blackjack_gui.py
@@ -1,1 +1,3 @@
 import tkinter as tk
+from project import create_deck, deal_card, calc_score, check_blackjack, compare_hands, render_card
+# this is the GUI of the project, while project.py is the brain
