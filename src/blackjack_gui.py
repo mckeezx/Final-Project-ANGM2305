0e2a6b4203000b57a3ef67925bb508e4b86cdfee
Zachary McKee <mckeezx@gmail.com>
2026-04-21 13:17:46 -0500

    Outlined window and display set up

--- src/blackjack_gui.py
+++ src/blackjack_gui.py
@@ -1,3 +1,17 @@
 import tkinter as tk
 from project import create_deck, deal_card, calc_score, check_blackjack, compare_hands, render_card
 # this is the GUI of the project, while project.py is the brain
+
+# window set up
+# tk.Tk()
+# 960x540
+# black bkg
+
+#display set up 
+# bkg black
+# fg green
+# courier font
+# disabled state so no typing
+# display pack pady=10
+
+#button set up
